0bd29eb533f711881b29f5107cfd26f6e82604cc
RamisaFatima2005 <163189004+RamisaFatima2005@users.noreply.github.com>
2025-01-22 18:40:49 +0500

    Update page.tsx

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -3,40 +3,36 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-    <div className='bg-black' >
-    
+      <div className='bg-black'>
+        {/* Intro */}
+        <div className="p-8 sm:p-16 md:p-32">
+          <div className="text-white text-base sm:text-xl md:text-2xl flex justify-start w-full md:w-96 h-auto">
+            <p>
+              Hii, I&apos;m <b>Ramisa Fatima,</b>
+              <br />
+              <br />
+              an aspiring AI engineer with a
+              <br /> passion for building clean, responsive
+              <br /> web applications. Dive into my work
+              <br /> and see how I blend creativity with
+              <br /> technology.
+            </p>
+          </div>
 
-    {/*Intro*/}
-    <div className="p-32">
-    <div className="text-white text-2xl flex justify-start w-96 h-7">
-    <p>Hii, I&apos;m <b>Ramisa Fatima,</b>
-    <br/>
-    <br/>
-    an aspiring AI engineer with a
-    <br/> passion for building clean, responsive
-    <br/> web applications. Dive into my work
-     <br/>and see how I blend creativity with 
-     <br/>technology.</p>
-    </div>
-
-
-    {/*Image*/}
-<div className="flex justify-end">
-        <div>
-          <Image
-            src="/Profile.webp"
-            alt="Profile Picture"
-            width={250}
-            height={250}
-            className="flex rounded-full border-white border-4 shadow-lg"
-          />
+          {/* Image */}
+          <div className="flex justify-center md:justify-end mt-8 md:mt-0">
+            <div>
+              <Image
+                src="/Profile.webp"
+                alt="Profile Picture"
+                width={250}
+                height={250}
+                className="rounded-full border-white border-4 shadow-lg"
+              />
+            </div>
+          </div>
         </div>
       </div>
-
-      </div>
-
-      </div>
-
     </>
   );
 }
