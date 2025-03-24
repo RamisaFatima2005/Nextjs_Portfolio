f31e419b465fe9d5977e806e0983c8ae5162fad9
Ramisa Fatima <163189004+RamisaFatima2005@users.noreply.github.com>
2025-03-24 14:14:27 +0500

    Update page.tsx

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,38 +1,12 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <>
-      <div className='bg-black'>
-        {/* Intro */}
-        <div className="p-8 sm:p-16 md:p-32">
-          <div className="text-white text-base sm:text-xl md:text-2xl flex justify-start w-full md:w-96 h-auto">
-            <p>
-              Hii, I&apos;m <b>Ramisa Fatima,</b>
-              <br />
-              <br />
-              an aspiring AI engineer with a
-              <br /> passion for building clean, responsive
-              <br /> web applications. Dive into my work
-              <br /> and see how I blend creativity with
-              <br /> technology.
-            </p>
-          </div>
-
-          {/* Image */}
-          <div className="flex justify-center md:justify-end mt-8 md:mt-0">
-            <div>
-              <Image
-                src="/Profile.webp"
-                alt="Profile Picture"
-                width={250}
-                height={250}
-                className="rounded-full border-white border-4 shadow-lg"
-              />
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
-}
+<div className="flex flex-col-reverse md:flex-row items-center justify-between">
+  <div className="text-white">
+    <h1 className="text-3xl font-bold">Hii, I'm <span className="font-bold">Ramisa Fatima,</span></h1>
+    <p className="mt-2">an aspiring AI engineer with a passion for building clean, responsive web applications. 
+    Dive into my work and see how I blend creativity with technology.</p>
+  </div>
+  <img 
+    src="your-image.jpg" 
+    alt="Profile" 
+    className="w-40 h-40 rounded-full object-cover md:order-last"
+  />
+</div>
