2ae5d3eb115f030d0b10bbc4ee417cdbe9be39b2
Ramisa Fatima <163189004+RamisaFatima2005@users.noreply.github.com>
2025-03-24 14:19:04 +0500

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,45 +1,40 @@
-import React from 'react'
-import Image from 'next/image'
+import React from 'react';
+import Image from 'next/image';
 
 export default function About() {
   return (
     <>
       <div className='bg-black text-white'>
-        {/* About */}
-        <div className="p-8 sm:p-16 md:p-28">
-          <h1 className='text-3xl sm:text-4xl md:text-5xl flex justify-start text-white'>
-            <b><i>About Me</i></b>
-          </h1>
-          <br />
-          <br />
-          <div className="text-white text-lg sm:text-xl md:text-2xl flex justify-start w-full md:w-96 h-auto">
+        {/* About Section */}
+        <div className="p-8 sm:p-16 md:p-28 flex flex-col md:flex-row items-center md:items-start justify-between">
+          
+          {/* Text Content */}
+          <div className="text-lg sm:text-xl md:text-2xl w-full md:w-1/2">
+            <h1 className='text-3xl sm:text-4xl md:text-5xl text-white mb-6'>
+              <b><i>About Me</i></b>
+            </h1>
             <p>
-              I have experience working with 
-              <br />modern technologies like HTML, 
-              <br />CSS, TypeScript, and JavaScript, 
-              <br />and I&apos;m currently expanding my 
-              <br />skills in Next.js. I enjoy creating 
-              <br />clean, responsive, and user-
-              <br />friendly websites, and I&apos;m always 
-              <br />eager to learn new tools and 
-              <br />technologies.
+              I have experience working with modern technologies like HTML, 
+              CSS, TypeScript, and JavaScript, and I&apos;m currently expanding my 
+              skills in Next.js. I enjoy creating clean, responsive, and user-
+              friendly websites, and I&apos;m always eager to learn new tools and 
+              technologies.
             </p>
           </div>
 
-          {/* Image */}
-          <div className="flex justify-center md:justify-end mt-8 md:mt-0">
-            <div className="relative -mt-12 sm:-mt-16 md:-mt-24">
-              <Image
-                src="/About.PNG"
-                alt="About Me Picture"
-                width={250}
-                height={250}
-                className="rounded-lg shadow-lg"
-              />
-            </div>
+          {/* Image Section */}
+          <div className="mt-8 md:mt-0">
+            <Image
+              src="/About.PNG"
+              alt="About Me Picture"
+              width={250}
+              height={250}
+              className="rounded-lg shadow-lg"
+            />
           </div>
+
         </div>
       </div>
     </>
-  )
+  );
 }
