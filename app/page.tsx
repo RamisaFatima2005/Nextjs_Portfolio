a92de8ec145caf1a4b671289bc00e83065ba1ea9
Ramisa Fatima <163189004+RamisaFatima2005@users.noreply.github.com>
2025-04-22 23:53:01 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Home from './home/page'
 import About from './about/page'
-import Project from './projects/page'
+import ProjectsMainPage from './project/page'
 import ContactMe from './contact/page'
 
 function page() {
@@ -12,7 +12,7 @@ function page() {
     <hr className="border-gray-400 my-4" />
     <About/>
     <hr className="border-gray-400 my-4" />
-    <Project/>
+    <ProjectsMainPage/>
     <hr className="border-gray-400 my-4" />
     <ContactMe/>
     </div>
